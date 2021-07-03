3cdb4a23df58c8b931e2d05b1e432e31b329befd
johnnythemadman <barteqdev@gmail.com>
2021-07-03 15:50:16 +0200

    basic aiming

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,17 +4,13 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
     public Rigidbody2D body;
+    public Camera cam;
 
     private Vector2 movement;
+    private Vector2 mousePosition;
 
-    // Awake is called when the script instance is being loaded
-    // Init all dependencies
-    private void Awake()
-    {
-
-    }
-
-
+    public GameObject firePoint;
+    
     // Update is called once per frame
     void Update()
     {
@@ -23,23 +19,37 @@ public class PlayerMovement : MonoBehaviour
 
     void ProcessInputs()
     {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            GetComponent<Renderer>().material.color = Color.red;
-        }
-        else if (Input.GetKeyDown(KeyCode.F))
-        {
-            GetComponent<Renderer>().material.color = Color.green;
-        }
-
-        var moveX = Input.GetAxisRaw("Horizontal");
-        var moveY = Input.GetAxisRaw("Vertical");
-        movement = new Vector2(moveX, moveY).normalized;
-        // dummy
+        CalcMovement();
+        CalcMousePosition();
     }
 
     void FixedUpdate()
     {
+        UpdatePosition();
+        LookAt(mousePosition);
+    }
+
+    private void CalcMovement()
+    {
+        var moveX = Input.GetAxisRaw("Horizontal");
+        var moveY = Input.GetAxisRaw("Vertical");
+        movement = new Vector2(moveX, moveY).normalized;
+    }
+
+    private void CalcMousePosition()
+    {
+        mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void UpdatePosition()
+    {
         body.MovePosition(body.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    private void LookAt(Vector2 target)
+    {
+        var direction = target - new Vector2(firePoint.transform.position.x, firePoint.transform.position.y);
+        var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        body.rotation = angle;
     }
 }
